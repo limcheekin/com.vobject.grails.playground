840ce9cc71ed4886008267721512b24ad0a9bc3a
limcheekin <limcheekin@vobject.com>
2009-06-01 00:27:57 +0800

    First import of acegi plugin exploration and include cobertura code coverage to pom.xml

---
+++ grails-app/domain/RequestMap.groovy
@@ -0,0 +1,15 @@
+
+
+/**
+ * Request Map domain class.
+ */
+class RequestMap {
+
+	String url
+	String configAttribute
+
+	static constraints = {
+		url(blank: false, unique: true)
+		configAttribute(blank: false)
+	}
+}
